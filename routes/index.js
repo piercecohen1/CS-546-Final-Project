e5860a48e2775463e514e3a61c9ddb1268fcb053
dcortinas <dcortina@stevens.edu>
2020-05-14 17:51:39 -0400

    More moving files from other branch

---
+++ routes/index.js
@@ -0,0 +1,124 @@
+const pollsRoutes = require('./polls');
+const tipsRoutes = require('./tips');
+const whattodoRoutes = require('./whattodo');
+const userData = require('../data/users');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const saltRounds = 5;
+
+const constructorMethod = (app) => {
+  app.use('/polls', pollsRoutes);
+	app.use('/tips', tipsRoutes);
+	app.use('/whattodo', whattodoRoutes);
+
+	app.post('/login', async (req, res) => {
+		const data = req.body;
+		if(!data || !data.username || !data.password){
+			res.status(400).render('login', {error: true});
+		}else{
+			const allUsers = await userData.getAllUsers();
+			var i;
+			var uname;
+			for (i = 0; i < allUsers.length; i++) {
+				tuser = allUsers[i];
+				if (tuser.userName === data.username){
+					uname = tuser;
+				}else{
+					uname = undefined;
+				}
+			}
+			try{
+				if(uname == undefined){
+					return res.status(401).render('pages/login', {error: true});
+				}
+			}catch(e){
+				return res.status(401).render('pages/login', {error: true});
+			}
+			let compare = false;
+			try{
+				compare = await bcrypt.compare(data.password, uname.hashedPassword);
+			}catch(e){
+				return res.status(401).render('pages/login', {error: true});
+			}
+			if(!compare){
+				res.status(401).render('pages/login', {error: true});
+			}else{
+				req.session.user = uname;
+				req.session.AuthCookie = true;
+				return res.render('pages/home');
+			}
+		}
+	});
+
+	app.post('/signup', async (req, res) => {
+		const data = req.body;
+		if(!data || !data.username || !data.password || !data.email){
+			res.status(400).render('pages/login', {serror: true});
+		}else{
+			const allUsers = await userData.getAllUsers();
+			var i;
+			for (i = 0; i < allUsers.length; i++) {
+				tuser = allUsers[i];
+				if (tuser.userName == data.username){
+					var uname = tuser;
+				}
+			}
+			if(uname != undefined){
+				res.status(401).render('pages/login', {serror: true});
+			}else{
+				const hash = await bcrypt.hash(data.password, saltRounds);
+				const user = await userData.addUser(data.username, hash, data.email);
+
+				req.session.user = user;
+				req.session.AuthCookie = true;
+				return res.render('pages/home');
+			}
+		}
+	});
+
+	app.get('/logout', (req, res) => {
+		if(!req.session.AuthCookie){
+			res.render('pages/login', {error: false});
+		}else{
+			const anHourAgo = new Date();
+			anHourAgo.setHours(anHourAgo.getHours() - 1);
+			res.clearCookie('AuthCookie');
+			res.clearCookie('user');
+			req.session.destroy();
+			res.render('pages/logout');
+		}
+	});
+
+	app.get('/delete', async (req, res) => {
+		if(!req.session.AuthCookie){
+			res.render('pages/login', {error: false});
+		}else{
+			const teuser = await userData.removeUser(req.session.user._id);
+			const anHourAgo = new Date();
+			anHourAgo.setHours(anHourAgo.getHours() - 1);
+			res.clearCookie('AuthCookie');
+			res.clearCookie('user');
+			req.session.destroy();
+			res.render('pages/login', {deleted: true});
+		}
+	});
+
+  app.get('/', (req, res) => {
+		if(!req.session.AuthCookie){
+			res.render('pages/login', {error: false});
+		}else{
+			res.render('pages/home', {error: false});
+		}
+  	});
+
+	app.use('*', (req, res) => {
+		if(!req.session.AuthCookie){
+			res.render('pages/login', {error: false});
+		}else{
+			res.render('pages/home', {error: false});
+		}
+	});
+
+};
+
+module.exports = constructorMethod;
---
+++ routes/tips.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const data = require('../data');
+
+const router = express.Router();
+const tipData = require('../data/tips');
+
+router.get('/', async (req, res) => {
+    const tipsList = await tipData.getAllTips();
+    res.render('pages/tips', {tips: tipsList});
+});
+
+router.get('/new', async (req, res) => {
+    res.render('pages/new/createtip');
+});
+
+router.post('/new', async (req, res) => {
+    const postdata = req.body;
+    if(!postdata || !postdata.title || !postdata.description){
+        res.status(400).render('pages/tips', {error: true});
+    }
+    try{
+      const {title, description} = postdata;
+      const newTip = await tipData.addTip(
+        title,
+        description
+      );
+
+      res.json(newTip);
+      // res.redirect('/tips');
+    } catch (e) {
+      res.status(500).json({error: e});
+    }
+    // res.send('partials/tips_item', {title: data.title, tip: data.description});
+
+});
+
+module.exports = router;
