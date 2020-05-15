af24d94507ba88924609d16daafaf21372c8bf5f
Matthew Jaros <mjaros1@stevens.edu>
2020-05-14 23:07:38 -0400

    Added seeds to the seed file

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,14 +1,17 @@
 const dbConnection = require('../config/mongoConnection');
 const data = require('../data/');
+const bcrypt = require('bcryptjs');
 const users = data.users;
 const posts = data.posts;
+const tips = data.tips;
 
 async function main() {
   const db = await dbConnection();
 
   await db.dropDatabase();
 
-  const patrick = await users.addUser('Patrick', 'Hill');
+  const hash = await bcrypt.hash("Hill", 5);
+  const patrick = await users.addUser('Patrick', hash);
   const id = patrick._id;
 
   await posts.addPost('Hello, class!', 'Today we are creating a blog!', [], id);
@@ -26,10 +29,41 @@ async function main() {
     id
   );
 
-  const aiden = await users.addUser('Aiden', 'Hill');
+  await posts.addPost(
+    'Great new post',
+    'Wow this post sure is great I am glad I posted it',
+    [],
+    id
+  );
+  await posts.addPost(
+    'Political post',
+    'This is a hypothetical angry political post that should be deleted by the mpds',
+    [],
+    id
+  );
+  const hash2 = await bcrypt.hash("Hill2", 5);
+  const aiden = await users.addUser('Aiden', hash2);
   await posts.addPost(
     "Aiden's First Post",
     "I'm 6 months old, I can't blog1",
+    [],
+    aiden._id
+  );
+  await tips.addTip(
+    "Tip header",
+    "Tip description",
+    [],
+    aiden._id
+  );
+  await tips.addTip(
+    "Tip yup this is a tip",
+    "Tip haha yeah this is a tip description",
+    [],
+    aiden._id
+  );
+  await tips.addTip(
+    "third tips the charm",
+    "Tip of the morning to yah",
     [],
     aiden._id
   );
