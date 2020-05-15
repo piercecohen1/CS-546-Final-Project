468776ccab83af20ee0ab236dbcf6875f5e826c7
Pierce Cohen <pbrinker@stevens.edu>
2020-05-14 21:44:01 -0400

    Restructure pages in views. Edit navigation bar

--- routes other branch/index.js
+++ routes other branch/index.js
@@ -1,16 +1,16 @@
-const postRoutes = require('./posts');
+const tipRoutes = require('./tips');
 const userRoutes = require('./users');
 const path = require('path');
 
 const constructorMethod = (app) => {
-  app.use('/posts', postRoutes);
+  app.use('/tips', tipRoutes);
   app.use('/users', userRoutes);
   app.get('/about', (req, res) => {
     res.sendFile(path.resolve('static/about.html'));
   });
 
   app.use('*', (req, res) => {
-    res.redirect('/posts');
+    res.redirect('/home');
   });
 };
 
--- routes other branch/posts.js
+++ routes other branch/posts.js
@@ -1,69 +1,69 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
-const tipData = data.tips;
+const postData = data.posts;
 const userData = data.users;
 
 router.get('/new', async (req, res) => {
   const users = await userData.getAllUsers();
-  res.render('tips/new', { users: users });
+  res.render('posts/new', { users: users });
 });
 
 router.get('/:id', async (req, res) => {
   try {
-    const tip = await tipData.getTipById(req.params.id);
-    res.render('tips/single', { tip: tip });
+    const post = await postData.getPostById(req.params.id);
+    res.render('posts/single', { post: post });
   } catch (e) {
     res.status(500).json({ error: e });
   }
 });
 
 router.get('/tag/:tag', async (req, res) => {
-  const tipList = await tipData.getTipsByTag(req.params.tag);
-  res.render('pages/index', { tips : tipList });
+  const postList = await postData.getPostsByTag(req.params.tag);
+  res.render('posts/index', { posts: postList });
 });
 
 router.get('/', async (req, res) => {
-  const tipList = await tipData.getAllTips();
-  res.render('pages/index', { tips: tipList });
+  const postList = await postData.getAllPosts();
+  res.render('posts/index', { posts: postList });
 });
 
 router.post('/', async (req, res) => {
-  let tipPostData = req.body;
+  let blogPostData = req.body;
   let errors = [];
 
-  if (!tipPostData.title) {
+  if (!blogPostData.title) {
     errors.push('No title provided');
   }
 
-  if (!tipPostData.body) {
+  if (!blogPostData.body) {
     errors.push('No body provided');
   }
 
-  if (!tipPostData.posterId) {
+  if (!blogPostData.posterId) {
     errors.push('No poster selected');
   }
 
   if (errors.length > 0) {
     const users = await userData.getAllUsers();
-    res.render('tips/new', {
+    res.render('posts/new', {
       errors: errors,
       hasErrors: true,
-      tip: tipPostData,
+      post: blogPostData,
       users: users
     });
     return;
   }
 
   try {
-    const newTip = await tipData.addTip(
-      tipPostData.title,
-      tipPostData.body,
-      tipPostData.tags || [],
-      tipPostData.posterId
+    const newPost = await postData.addPost(
+      blogPostData.title,
+      blogPostData.body,
+      blogPostData.tags || [],
+      blogPostData.posterId
     );
 
-    res.redirect(`/tips/${newTip._id}`);
+    res.redirect(`/posts/${newPost._id}`);
   } catch (e) {
     res.status(500).json({ error: e });
   }
@@ -72,14 +72,14 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   let updatedData = req.body;
   try {
-    await tipData.getTipById(req.params.id);
+    await postData.getPostById(req.params.id);
   } catch (e) {
-    res.status(404).json({ error: 'Tip not found' });
+    res.status(404).json({ error: 'Post not found' });
     return;
   }
   try {
-    const updatedTip = await tipData.updateTip(req.params.id, updatedData);
-    res.json(updatedTip);
+    const updatedPost = await postData.updatePost(req.params.id, updatedData);
+    res.json(updatedPost);
   } catch (e) {
     res.status(500).json({ error: e });
   }
@@ -87,14 +87,14 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    await tipData.getTipById(req.params.id);
+    await postData.getPostById(req.params.id);
   } catch (e) {
-    res.status(404).json({ error: 'Tip not found' });
+    res.status(404).json({ error: 'Post not found' });
     return;
   }
 
   try {
-    await tipData.removeTip(req.params.id);
+    await postData.removePost(req.params.id);
     res.sendStatus(200);
   } catch (e) {
     res.status(500).json({ error: e });
